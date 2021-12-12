3d84f1f7fc6baf0e0691c257af25e3b066d548f7
SURAJ SAFI <94732511+suraj-safi1@users.noreply.github.com>
2021-12-12 12:51:53 +0530

    LOOP

---
+++ while_loop_table.py
@@ -0,0 +1,8 @@
+#print table by whilw loop
+thislist = [1,2,3,4,5,6,7,8,9,10]
+i = 0
+b=int(input("Enter your number to print table:\n"))
+while i <= len(thislist):
+  print(thislist[i]*b)
+  i = i + 1
+
